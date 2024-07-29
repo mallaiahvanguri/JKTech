fdfe5bf669149bd288c24c395501a0efb8caef2d
MALLAIAH VAGURI <mallaiah.v@gmail.com>
2024-07-30 00:38:45 +0530

    Database configuration functions added and configured into the application

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from http import HTTPStatus
+from database import config_postgre_sql, create_all_db
 
 app = Flask(__name__)
 
@@ -7,5 +8,7 @@ app = Flask(__name__)
 def index():
     return {"msg":"Hello world"}, HTTPStatus.OK
 
+config_postgre_sql(app)
+create_all_db(app)
 if __name__ == "__main__":
     app.run(debug=True)
